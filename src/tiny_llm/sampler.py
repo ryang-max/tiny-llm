e7ea44851f3cbb13e92855435b4003856ad27097
ryang-max <y1cunhui.yang@gmail.com>
2025-06-20 17:43:37 +0800

    day 7

--- src/tiny_llm/sampler.py
+++ src/tiny_llm/sampler.py
@@ -4,8 +4,21 @@ import copy
 
 def make_sampler(temp: float, top_p: float, top_k: int | None):
     def sample(logprobs: mx.array):
+        if top_k is not None:
+            mask_index = mx.argpartition(
+                -logprobs, kth=top_k, axis=-1
+            )[:, top_k:]
+            logprobs[:, mask_index] = -mx.inf
+        if top_p is not None:
+            sorted_idx = mx.argsort(-logprobs, axis=-1)
+            sorted_logprobs = logprobs[:, sorted_idx]
+            cumsum = mx.cumsum(sorted_logprobs, axis=-1)
+            logprobs[:, sorted_idx] = mx.where(cumsum < top_p, sorted_logprobs, -mx.inf)
         if temp == 0:
             return mx.argmax(logprobs, axis=-1)
-        pass
+        else:
+            return mx.random.categorical(
+                logprobs / temp, axis=-1
+            )
 
     return sample
